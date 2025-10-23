373f36b14ff2544ce72081464b0581a0193bd401
Harry Kim <ck3747@stern.nyu.edu>
2025-10-23 11:48:37 -0400

    pandas added & simple line chart using demo data

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+v0.2
+- Panda library added
+- Demo data (csv) added
+- Simple line chart using the demo data
+
+v0.1 
+- Empty Streamlit app
--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Stock Stremer by Harry Kim")
+st.title("Stock price vs time")
 
+# load demo data to a dataframe
+df = pd.read_csv('./resources/demo_stock_data.csv', parse_dates=["ts"])
 
+# get unique symbols
+symbols = df["symbol"].unique().tolist()
+
+# sidemenu
+st.sidebar.header("Menu")
+symbolSelected = st.sidebar.selectbox("Symbol", symbols, index=0)
+
+# filter dataframe by selected symbol and sort by timestamp
+df_sym = df[df["symbol"] == symbolSelected].sort_values("ts").set_index("ts")
+
+# line chart of price vs timestamp
+st.subheader(f"{symbolSelected} price over time")
+st.line_chart(df_sym["price"])
